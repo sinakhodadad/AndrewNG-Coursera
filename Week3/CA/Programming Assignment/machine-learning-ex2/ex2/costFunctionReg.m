ce70c6a5ba7146fe033558595be970e9b965893f
sinahk <sinahk1992@yahoo.com>
2019-07-30 16:38:59 +0430

    week 3

---
+++ Week3/CA/Programming Assignment/machine-learning-ex2/ex2/costFunctionReg.m
@@ -0,0 +1,34 @@
+﻿function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+
+%hypothesis
+h_theta = sigmoid(X * theta);
+
+%regularized cost function (theta 0 not adjusted)
+J = (1/m) * sum((-y .* log(h_theta)) - (1 - y) .* log(1 - h_theta)) + ((lambda / (2 * m)) * sum(realpow(theta(2:end),2))); 
+
+
+%gradient_decent
+theta_reg = theta;
+theta_reg(1) = 0;
+grad = (1 / m) * (X' * (h_theta - y) + (lambda * theta_reg));
+
+% =============================================================
+
+end
